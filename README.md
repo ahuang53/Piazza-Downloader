5c31291eced6b772534842ef372e62f24b6b88ed
Matthew So <matthewdyso@gmail.com>
2024-10-04 13:52:10 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Piazza-Downloader
+
+## Setup:
+Install Piazza API https://github.com/hfaran/piazza-api and any other missing libraries
+
+## Use:
+1. Navigate to the piazza course that you would like to download on your browser and go to the resources tab.
+2. Inspect the page (right click -> inspect or F12). 
+3. In the top-middle of the inspect dialog, click Sources
+4. Download the resources file as an html file and save it into this folder
+5. Run main.py and all files will be in the downloaded_resources folder
+Make sure to choose the correct course that you are downloading as different courses will have different resources.html files. 
+
+Some of the section names may be named correctly so review the folders and modify to your liking. 
+
+## Disclaimer for Unofficial Piazza Downloader
+### Important Notice:
+
+This tool is an unofficial downloader for Piazza, created for educational and personal use only. It is not affiliated with, endorsed by, or sponsored by Piazza or any of its associated organizations.
+
+### Usage Guidelines:
+
+The downloader is intended solely for the retrieval of content you have the right to access.
+Users are responsible for ensuring compliance with Piazza's terms of service, community guidelines, and copyright regulations.
+Downloading or distributing content without proper authorization may violate intellectual property laws or the rights of others.
+Risks:
+
+By using this tool, you acknowledge that there may be risks involved, including potential changes to Piazza's interface, functionality, or terms of service, which could affect the downloader's performance.
+Use this tool at your own risk. The developers do not assume any liability for any damages or issues arising from its use.
+Privacy and Security:
+
+The downloader may require your Piazza credentials for authentication. Please use caution and ensure your information is secure.
+The developers do not store or share any personal information or credentials.
+By using this unofficial downloader, you agree to these terms and acknowledge your responsibility to use it appropriately.
+
---
+++ main.py
@@ -0,0 +1,128 @@
+from pyquery import PyQuery as pq
+import os
+import json
+import re
+import requests
+from piazza_api import Piazza
+from piazza_api.rpc import PiazzaRPC
+
+from piazza_api.exceptions import AuthenticationError, NotAuthenticatedError, \
+    RequestError
+
+piazza_url = 'https://piazza.com/class_profile/get_resource/'
+classID = ''
+
+'''Modified User Login function from piazza-api to login to piazza and
+access resource files and download them with requests'''
+
+def download(email, password, sections):
+    """Login with email, password and get back a session cookie
+
+    :type  email: str
+    :param email: The email used for authentication
+    :type  password: str
+    :param password: The password used for authentication
+    """
+    session = requests.Session()
+    # Need to get the CSRF token first
+    response = session.get('https://piazza.com/main/csrf_token')
+
+    # Make sure a CSRF token was retrieved, otherwise bail
+    if response.text.upper().find('CSRF_TOKEN') == -1:
+        raise AuthenticationError("Could not get CSRF token")
+    
+    # Remove double quotes and semicolon (ASCI 34 & 59) from response string.
+    # Then split the string on "=" to parse out the actual CSRF token
+    csrf_token = response.text.translate({34: None, 59: None}).split("=")[1]
+
+
+    # Log in using credentials and CSRF token and store cookie in session
+    response = session.post(
+        'https://piazza.com/class', 
+        data=r'from=%2Fsignup&email={0}&password={1}&remember=on&csrf_token={2}'.format(email, password, csrf_token)
+    )
+
+    # If non-successful http response, bail
+    if response.status_code != 200:
+        raise AuthenticationError(f"Could not authenticate.\n{response.text}")
+    
+    # Piazza might give a successful http response even if there is some other
+    # kind of authentication problem. Need to parse the response html for error message
+    pos = response.text.upper().find('VAR ERROR_MSG')
+    errorMsg = None
+    if pos != -1:
+        end = response.text[pos:].find(';')
+        errorMsg = response.text[pos:pos+end].translate({34: None}).split('=')[1].strip()
+
+    if errorMsg is not None:
+        raise AuthenticationError(f"Could not authenticate.\n{errorMsg}")
+    
+    
+    current_directory = os.getcwd()
+
+    #Retrieve all files in each section and download the files 
+    for key in sections.keys():
+        final_directory = os.path.join(current_directory, "downloaded_resources", key)
+        if not os.path.exists(final_directory):
+            os.makedirs(final_directory)
+
+        for item in sections[key]:
+            print(item[1])
+            
+            # Open URL and read contents using requests
+            page_response_obj = session.get(item[1])
+            
+             # Check if the request was successful
+            if page_response_obj.status_code == 200:
+                pdf_filename = os.path.join(final_directory, f"{item[0]}.pdf")
+                # Write the PDF content to a file
+                with open(pdf_filename, 'wb') as pdf_file:
+                    pdf_file.write(page_response_obj.content)
+                print(f"Downloaded and saved: {pdf_filename}")
+                
+            else:
+                print(f"Failed to download {item[0]}: {page_response_obj.status_code}")
+
+if __name__ == "__main__":
+    
+    #use Piazza API to get classID
+    p = Piazza()
+    
+    username = input("Email: ")
+    password = input("Password: ")
+
+    p.user_login(username,password)
+    classes = p.get_user_classes()
+
+    for i in range(len(classes)):
+        print("{}. {}".format((i+1), classes[i]['name']))
+
+    idx = int(input("Which class are you downloading from: ")) - 1
+    classID = classes[idx]['nid']
+    
+    #use pyquery to access the resources file
+    doc = pq(filename = 'resources.html')
+    script_content = doc('script').text()
+    resources_json_str = re.search(r'var RESOURCES = (\[.*?\]);', script_content, re.DOTALL).group(1)
+    json_object = json.loads(resources_json_str)
+    out_file = open("allResources.json", "w")
+    json.dump(json_object, out_file, indent = 6)
+    out_file.close()
+
+    #create dictionary containing all sections and the corresponding material
+    sections = {}
+
+    for item in json_object:
+        #section type
+        sectionKey = item['config']['section']
+        
+        #create URL for material
+        url = piazza_url + classID + '/' + item['id']
+      
+        if sectionKey not in sections.keys():
+            sections[sectionKey] = []
+                
+        sections[sectionKey].append((item['subject'], url))
+
+    download(username,password,sections)
+    
